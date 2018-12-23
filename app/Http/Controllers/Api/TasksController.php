a62185244358ff15405d23c168c74c86e5462378
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-12-23 13:19:38 -0300

    fix(tasks): fixed task services class name

--- app/Http/Controllers/Api/TasksController.php
+++ app/Http/Controllers/Api/TasksController.php
@@ -2,22 +2,31 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Task;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\Contracts\TaskRepository;
 
 class TasksController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * @var TaskRepository
+     */
+    protected $repository;
+
+    public function __construct(TaskRepository $repository){
+        $this->repository = $repository;
+    }
+
+    /**
+     * Display a listing of tasks with pagination.
      *
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
     public function index(Request $request)
     {
-        $limit = $request->get('limit', '10');
-        $tasks = Task::paginate($limit)->toArray();
+        $limit = $request->limit ?? 10;
+        $tasks = $this->repository->skipPresenter()->paginate($limit);
 
         return response()->json($tasks, 200);
     }
