9330ffc492fa3faed585513908075aee18f19656
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-12-24 11:00:45 -0300

    syntax(tasks): removed comments in task services code

--- app/Services/TaskServices.php
+++ app/Services/TaskServices.php
@@ -2,18 +2,18 @@
 
 namespace App\Services;
 
-//use App\Repositories\Contracts\TaskRepository;
-//use App\Validators\TaskValidator;
+use App\Repositories\Contracts\TaskRepository;
+use App\Validators\TaskValidator;
 
 class TaskServices
 {
-//    private $taskRepository;
-//    private $taskValidator;
-//
-//    public function __construct(TaskRepository $taskRepository, TaskValidator $taskValidator)
-//    {
-//        $this->taskRepository = $taskRepository;
-//        $this->taskValidator = $taskValidator;
-//    }
+    private $taskRepository;
+    private $taskValidator;
+
+    public function __construct(TaskRepository $taskRepository, TaskValidator $taskValidator)
+    {
+        $this->taskRepository = $taskRepository;
+        $this->taskValidator = $taskValidator;
+    }
 
 }
