b70c93853de1da9f744e481ba525dbaa7efaa40b
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-12-27 00:24:18 -0300

    feat(app): add script to create a new db for testing and created env for testing

--- linux-run.sh
+++ linux-run.sh
@@ -5,6 +5,7 @@ docker-compose up -d
 
 echo [+] Copying the configuration example file
 docker exec -it task-manager-app cp .env.example .env
+docker exec -it task-manager-app cp .env.example.testing .env.testing
 
 echo [+] Installing the dependencies
 docker exec -it task-manager-app composer install
@@ -12,11 +13,16 @@ docker exec -it task-manager-app composer install
 echo [+] Generating key
 docker exec -it task-manager-app php artisan key:generate
 
+sleep 5
+
+echo [+] Creating database for tests
+docker exec -it task-manager-mysql mysql -u root -ptaskmanager_pass -e "CREATE DATABASE IF NOT EXISTS taskmanager_db_testing;" -f
+
+echo [+] Ensuring user access to test database
+docker exec -it task-manager-mysql mysql -u root -ptaskmanager_pass -e "GRANT ALL ON taskmanager_db_testing.* TO 'taskmanager_user'@'%' IDENTIFIED BY 'taskmanager_pass';"
+
 echo [+] Making migrations
 docker exec -it task-manager-app php artisan migrate
-
-echo [+] Making seeds
-docker exec -it task-manager-app php artisan db:seed
 
 echo [+] Information of new containers
 docker ps -a
